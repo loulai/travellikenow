85d9f7f664b5c34f77d4927206e5603e32cf8b38
loulai <louiselai88@gmail.com>
2015-08-17 20:51:56 -0400

    introduced concept of country

---
+++ app/controllers/countries_controller.rb
@@ -0,0 +1,21 @@
+class CountriesController < ApplicationController
+	def new
+	end
+
+	def create
+		@country = Country.new(country_params)
+
+		@country.save
+		redirect_to @country
+	end
+
+	def show
+		@country = Country.find(params[:id])
+	end
+
+	private
+		def country_params
+			params.require(:country).permit(:title)
+		end
+
+end
